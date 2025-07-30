5a5e2d31ce4edec7b6b1ae7c7b83a02f25f2bf12
Thomas H. <vhhorne2@gmail.com>
2025-07-30 17:57:46 -0400

    initial commit

---
+++ src/player/player_state.gd
@@ -0,0 +1,14 @@
+class_name State 
+extends Node
+
+var player: CharacterBody2D
+var active: bool
+
+func on_enter():
+	pass
+	
+func on_leave():
+	pass
+
+func process(delta: float) -> void:
+	pass
